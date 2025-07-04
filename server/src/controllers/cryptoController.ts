0a24abcc700dad72e2ceb7b9920920ed3a9045f6
Yann Adem <yann.adem@gmail.com>
2025-07-04 18:38:49 +0100

    feat: fetch BTCUSD lastTrade from Kraken API

--- server/src/controllers/cryptoController.ts
+++ server/src/controllers/cryptoController.ts
@@ -1,9 +1,41 @@
-import {Request,Response} from 'express';
+import { Request, Response } from 'express';
+import axios from 'axios';
 
-const getCryptoData = (req: Request, res: Response) => {
+const pair: string = 'BTCUSD';
+const assetTickerInfo: string = 'XXBTZUSD';
 
-  res.json({message: 'Incoming Crypto Data!'});
+interface KrakenTickerResponse {
+  error: string[];
+  result: {
+    [assetTickerInfo: string]: {
+      c: string[];
+    };
+  };
 
-};
+}
+
+const getCryptoData = async (req: Request, res: Response): Promise<void> => {
+
+  try {
+
+    const response = await axios.get<KrakenTickerResponse>(`https://api.kraken.com/0/public/Ticker?pair=${pair}`);
+
+    const data = response.data;
+    const lastTrade = data.result?.XXBTZUSD?.c[0]; // last trade closed
+
+    if(!lastTrade){
+      res.status(404).json('Price not found');
+    }
+
+    res.status(200).json({pair, lastTrade});
+
+  } catch (error) {
+
+    console.log(`getCryptoData error: ${error}`);
+    res.status(500).json({ error: `Failed to fetch ${pair}` });
+
+  }
+}
 
 export default getCryptoData;
+
